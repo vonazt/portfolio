7ff965b66582b00b63fb7d55f6f7b95578b3a6f0
Richard Tzanov <richard.tzanov@jdlt.co.uk>
2020-07-21 21:01:48 +0100

    add Gaming component

---
+++ src/components/Gaming.tsx
@@ -0,0 +1,22 @@
+import React, { Fragment } from "react";
+
+export const Gaming = () => (
+  <Fragment>
+    <p className="text-white">
+      I couldn't think of any sensible collection to link to for my gaming, so
+      here's my top 10 games in no particular order:
+    </p>
+    <ol className="list-decimal text-white">
+      <li>Panzer Dragoon Saga</li>
+      <li>Day of the Tentacle</li>
+      <li>DOOM</li>
+      <li>God of War</li>
+      <li>Final Fantasy VII</li>
+      <li>Bioshock</li>
+      <li>Witcher 3</li>
+      <li>Half-Life 2</li>
+      <li>Portal 2</li>
+      <li>Legend of Zelda: Ocarina of Time</li>
+    </ol>
+  </Fragment>
+);
